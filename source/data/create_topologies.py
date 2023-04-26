88500bbd3a0591f1e4fa6674cb31c1bff1a6dff5
Isabel R. Armour-Garb <ia3026@della8.princeton.edu>
2023-04-25 21:38:51 -0400

    c_min

--- source/data/create_topologies.py
+++ source/data/create_topologies.py
@@ -2,6 +2,15 @@ import random
 import networkx as nx
 import pandas as pd
 
+
+import os
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 2000
+# get_ipython().system('pip3 install pygraphviz')
+
 def max_flow_computation(Gbase, capacity_max):
     max_capacities = dict(zip(list(Gbase.edges), [capacity_max] * len(list(Gbase.edges))))
     nx.set_edge_attributes(Gbase, max_capacities, 'capacity')
@@ -26,16 +35,31 @@ def process_graph_from_topology(file_path,
                                 sample_bounds,
                                 fp_max, 
                                 spectrum_size, 
-                                max_flow_flag=False):
+                                max_flow_flag=False,
+                                c_min_flag=False):
 
     # leases and fibers stuff
 
     Gbase = nx.read_gml(file_path)
+    #Gbase_cmin = find_edge_capacity_heuristic_centrality_betweeness(nx.DiGraph(Gbase))
+    # quit()
 
-    if max_flow_flag:
+    if max_flow_flag == False:
+        Gbase_cmin = find_edge_capacity_heuristic_centrality_betweeness(Gbase)
+    else:
         Gbase = nx.DiGraph(Gbase)
         max_flow_computation(Gbase, capacity_max)
-
+        if c_min_flag:
+            if c_min_flag == 1:
+                Gbase_cmin = find_edge_capacity_heuristic_centrality_betweeness(Gbase)
+            elif c_min_flag == 2:
+                Gbase_cmin = find_edge_capacity_heuristic_flow_betweeness(Gbase)
+            elif c_min_flag == 3:
+                Gbase_cmin = find_edge_capacity_heuristic_flow_communicability_centrality(Gbase)
+            else:
+                print('invalid c_min option')
+                quit()
+    
     source_list = []
     dest_list = []
     rtt_list = []
@@ -52,16 +76,30 @@ def process_graph_from_topology(file_path,
         source_list.append(edge[0])
         dest_list.append(edge[1])
         rtt_list.append(random.randint(rtt_min, rtt_max))
-        capacity_min_list.append(capacity_min)
+        # capacity_min_list.append(capacity_min)
+        #capacity_min_list.append(Gbase_cmin.edges[edge]['weight'])
         capacity_max_list.append(capacity_max)
         link_names_list.append("Link_{}".format(i))
 
+        # if c_min_flag:
+        #     capacity_min_list.append(Gbase_cmin.edges[edge]['weight'])
+        # else:
+        #     capacity_min_list.append(capacity_min)
         cos_list.append('BRONZE') # not entirely sure what this is
 
         if max_flow_flag:
             actual_capacity_list.append(Gbase.edges[edge]['actual_capacity'])
+            if c_min_flag:
+                capacity_min_list.append(Gbase_cmin.edges[edge]['weight']* Gbase.edges[edge]['actual_capacity'])
+            else:
+                capacity_min_list.append(capacity_min)
         else:
             actual_capacity_list.append(random.randint(*sample_bounds))
+            if c_min_flag:
+                capacity_min_list.append(Gbase_cmin.edges[edge]['weight']*random.randint(*sample_bounds))
+            else:
+                capacity_min_list.append(capacity_min)
+
 
         max_fp_list.append(fp_max)
         spectrum_size_ghz_per_fp_list.append(spectrum_size)
@@ -154,26 +192,100 @@ def process_graph_from_topology(file_path,
         ip_capacity_df.to_excel(writer, sheet_name='L3Links')
         spofs_df.to_excel(writer, sheet_name='Spofs')
 
+
+def find_edge_capacity_heuristic_centrality_betweeness(g):
+
+    # edge_count = defaultdict(int)
+    # m = nx.read_graphml(f).to_undirected()
+    
+    # g = nx.Graph(m)
+
+
+    cut_edges = nx.edge_betweenness_centrality(g, normalized=True)
+    max_val = max([val for val in cut_edges.values()])
+    min_val = min([val for val in cut_edges.values()])
+
+
+    for u, v, attr in g.edges(data=True):
+        if (u,v) in cut_edges.keys():
+            attr['weight'] = round((cut_edges[(u,v)]-min_val)/max_val,2)
+        else:
+            attr['weight'] = round((cut_edges[(v,u)]-min_val)/max_val,2)
+    return(g)
+
+
+def find_edge_capacity_heuristic_flow_betweeness(g):
+
+    # edge_count = defaultdict(int)
+    # m = nx.read_graphml(f).to_undirected()
+    
+    # g = nx.Graph(m)
+
+
+    cut_edges = nx.edge_current_flow_betweenness_centrality(g, normalized=True)
+    max_val = max([val for val in cut_edges.values()])
+    min_val = min([val for val in cut_edges.values()])
+
+
+    for u, v, attr in g.edges(data=True):
+        if (u,v) in cut_edges.keys():
+            attr['weight'] = round((cut_edges[(u,v)]-min_val)/max_val,2)
+        else:
+            attr['weight'] = round((cut_edges[(v,u)]-min_val)/max_val,2)
+    return(g)
+
+
+def find_edge_capacity_heuristic_flow_communicability_centrality(g):
+
+    #edge_count = defaultdict(int)
+    #m = nx.read_graphml(f).to_undirected()
+    
+    #g = nx.Graph(m)
+
+    cut_edges = {}
+    nodes = nx.communicability_exp(g)
+    for u, v, attr in g.edges(data=True):
+        cut_edges[(u,v)] = nodes[u][v]
+        
+    max_val = max([val for val in cut_edges.values()])
+    min_val = min([val for val in cut_edges.values()])
+
+
+    for u, v, attr in g.edges(data=True):
+        if (u,v) in cut_edges.keys():
+            attr['weight'] = round((cut_edges[(u,v)]-min_val)/max_val,2)
+        else:
+            attr['weight'] = round((cut_edges[(v,u)]-min_val)/max_val,2)
+    return(g)
+
+
 if __name__ == '__main__':
-    file_path = 'topologies/gml_files/Cogentco_with_label_unique.gml'
-    excel_path = 'topologies/random_sampled_topos/Cogentco_topology.xlsx'
-    rtt_min = 2
-    rtt_max = 5
-    capacity_min = 0
-    capacity_max = 1000
-    ip_capacity_final = 20
-    sample_bounds = (750, 950)
-    fp_max = 50
-    spectrum_size = 10
-    max_flow_flag = False
-    process_graph_from_topology(file_path, 
-                                excel_path, 
-                                rtt_min, 
-                                rtt_max, 
-                                capacity_min, 
-                                capacity_max, 
-                                ip_capacity_final,
-                                sample_bounds,
-                                fp_max, 
-                                spectrum_size,
-                                max_flow_flag)
+    for file in ['VisionNet_with_label_unique.gml', 'Globalcenter_with_label_unique.gml', 'Tinet_with_label_unique.gml', 'Cogentco_with_label_unique.gml', 'Kdl_with_label_unique.gml']:
+        f = '/scratch/gpfs/ia3026/cos561/neuroplan/source/data/topologies/' + file
+        file_path = 'topologies/gml_files/kdl_with_label_unique.gml'
+        excel_path = 'topologies/centrality_betweeness/KDL.xlsx'
+        rtt_min = 2
+        rtt_max = 5
+        capacity_min = 0
+        capacity_max = 1000
+        ip_capacity_final = 20
+        sample_bounds = (750, 950)
+        fp_max = 50
+        spectrum_size = 10
+        max_flow_flag = True
+        # c_min_flag = 1 for find_edge_capacity_heuristic_centrality_betweeness
+        # c_min_flag = 2 for find_edge_capacity_heuristic_flow_betweeness
+        # c_min_flag = 3 for find_edge_capacity_heuristic_flow_communicability_centrality
+        c_min_flag = 1
+        process_graph_from_topology(file_path, 
+                                    excel_path, 
+                                    rtt_min, 
+                                    rtt_max, 
+                                    capacity_min, 
+                                    capacity_max, 
+                                    ip_capacity_final,
+                                    sample_bounds,
+                                    fp_max, 
+                                    spectrum_size,
+                                    max_flow_flag,
+                                    c_min_flag)
